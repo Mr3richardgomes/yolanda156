c55efba7ea3e101b9bc40f98b00dca523a74caa3
Mr3richardgomes <mr3richardgomez@gmail.com>
2022-10-11 14:09:35 -0300

    feat : sub react-navigation --> screen nova solicitacao,Lista de Chamados

--- App.js
+++ App.js
@@ -4,16 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './src/screens/home/index';
 import { ChamadoScreen } from './src/screens/chamado/index';
-import { ChamadosScreen } from './src/screens/chamados/index';
 
 
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+
 
 const Stack = createNativeStackNavigator();
 
@@ -21,9 +14,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}}name="Home" component={HomeScreen} />
-        <Stack.Screen name="Chamado" component={ChamadoScreen} />
-        <Stack.Screen name="Chamados" component={ChamadosScreen} />
+        <Stack.Screen options={{ headerShown: false }}  name="Home" component={HomeScreen} />
+        <Stack.Screen options={{ title: '' ,headerBackTitle : 'voltar'} } name="Chamado" component={ChamadoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/chamado/Chamado.js
+++ src/screens/chamado/Chamado.js
@@ -1,21 +1,72 @@
 
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { ChamadosScreen } from '../chamados/index';
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  input: {
+    borderWidth: 1,
+    width: 300,
+    padding: 10
+  },
+  button: {
+    marginTop: 10,
+    padding: 15,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: 'black',
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0"
+  },
 })
 
+
+function StackScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>CHAMADO</Text>
+      <TextInput
+        multiline={true}
+        numberOfLines={10}
+        style={styles.input}
+        placeholder="Descreva sua Solicitação"
+
+      />
+      <Pressable style={styles.button} onPress={() => navigation.navigate('Chamado')}>
+        <Text style={styles.buttonText}>Abrir Chamado</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+}
+
+
+const Tab = createBottomTabNavigator();
+
+
+//function MyTabs() {
 export const ChamadoScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>CHAMADO</Text>
-      <Button
-        onPress={() => navigation.navigate('Chamado')}
-        title="ABRIR CHAMADO"
-        color="#841584"
-      />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="ChamadoSolicitacao" options={{ title: 'Nova Solicitação' }} component={StackScreen} />
+      <Tab.Screen name="ChamadosLista" options={{ title: 'Lista de Chamados' }} component={ChamadosScreen} />
 
+    </Tab.Navigator>
   );
-};
+}
+
+
 
